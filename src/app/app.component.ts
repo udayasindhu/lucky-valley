c8769f13ac7873951b2a973e655678fb64c9155b
Udaya Sindhu <udaya.sindhu@outlookc.om>
2020-08-19 15:35:10 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'localization';
+  userName: string;
+  playerNum: number;
+  luckyAlert: boolean;
+  luckyNumber: number;
+  unluckyAlert: boolean;
+
+  constructor(private translate : TranslateService){
+    translate.addLangs(['en','hi','kn','ml','ta','te'])
+    translate.setDefaultLang('en')
+  }
+
+  ngOnInit(){
+  }
+
+  switchLang(lang){
+    let language =  lang.target.value
+    this.translate.use(language);
+  }
+
+  getLuckyNumber(userName, playerNum) {
+    if (!playerNum.errors) {
+      this.luckyNumber = Math.floor(Math.random() * 10 + 1);
+      this.userName = userName.value;
+      this.playerNum = playerNum.value;
+      if (this.playerNum == this.luckyNumber) {
+        this.luckyAlert = true;
+        this.unluckyAlert = false;
+      } else {
+        this.luckyAlert = false;
+        this.unluckyAlert = true;
+      }
+    } else {
+      return;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
